d63ee9a4bf672618a91c0acc4a8afd5bcacaf84d
Jozef Lami <jozef@lami.sk>
2025-01-19 16:03:06 +0100

    Enum dispatch container that works

--- src/container_enum_dispatch.rs
+++ src/container_enum_dispatch.rs
@@ -1,40 +1,95 @@
-// use std::any::Any;
-// use std::collections::HashMap;
-// use std::sync::Arc;
-//
-// struct Container {
-//     element_type type,
-//     storage: HashMap<String, Option<Arc<dyn Any + Send + Sync>>>,
-// }
-//
-// impl crate::Container {
-//     fn new() -> crate::Container {
-//         crate::Container {
-//             storage: HashMap::new(),
-//         }
-//     }
-//
-//     fn set(&mut self, name: &str, instance: Arc<dyn Any + Send + Sync>) {
-//         self.storage.insert(name.to_string(), Some(instance));
-//     }
-//
-//     fn build<T: 'static + Send + Sync>(
-//         &mut self,
-//         name: &str,
-//         builder: fn(container: &mut crate::Container) -> Arc<T>,
-//     ) -> Arc<T> {
-//         match self.storage.get(name) {
-//             Some(a) => match a {
-//                 Some(i) => i.clone().downcast::<T>().unwrap(),
-//                 None => panic!("circular reference"),
-//             },
-//             None => {
-//                 self.storage.insert(name.to_string(), None);
-//                 let v = Arc::from(builder(self));
-//                 self.storage
-//                     .insert(name.to_string(), Some(Arc::clone(&v) as Arc<dyn Any + Send + Sync>));
-//                 v
-//             }
-//         }
-//     }
-// }
+use std::collections::HashMap;
+use std::sync::Arc;
+use uuid::Uuid;
+
+trait ServiceATrait: Send+Sync {
+    fn get_uuid(&self) -> Uuid;
+}
+
+struct ServiceA {
+    pub uuid: Uuid,
+}
+
+impl ServiceATrait for ServiceA {
+    fn get_uuid(&self) -> Uuid {
+        self.uuid
+    }
+}
+
+struct ServiceB {
+    service_a: Arc<ServiceA>,
+}
+
+enum ServiceEnum {
+    ServiceA(Arc<ServiceA>),
+    ServiceB(Arc<ServiceB>),
+}
+
+struct ContainerWithEnumDispatch {
+    storage: HashMap<String, Option<Arc<ServiceEnum>>>,
+}
+
+impl ContainerWithEnumDispatch {
+    fn new() -> ContainerWithEnumDispatch {
+        ContainerWithEnumDispatch {
+            storage: HashMap::new(),
+        }
+    }
+
+    fn set(&mut self, name: &str, instance: Arc<ServiceEnum>) {
+        self.storage.insert(name.to_string(), Some(instance));
+    }
+
+    fn build(
+        &mut self,
+        name: &str,
+        builder: fn(container: &mut ContainerWithEnumDispatch) -> ServiceEnum,
+    ) -> Arc<ServiceEnum> {
+        match self.storage.get(name) {
+            Some(a) => match a {
+                Some(i) => i.clone(),
+                None => panic!("circular reference"),
+            },
+            None => {
+                self.storage.insert(name.to_string(), None);
+                let v = Arc::from(builder(self));
+                self.storage.insert(name.to_string(), Some(v.clone()));
+                v
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Deref;
+    use uuid::Uuid;
+    use super::*;
+
+    fn service_a(c: &mut ContainerWithEnumDispatch) -> Arc<ServiceA> {
+        match c.build("service_a", |_container: &mut ContainerWithEnumDispatch| {
+            ServiceEnum::ServiceA(Arc::from(ServiceA{uuid: Uuid::new_v4()}))
+        }).deref() {
+            ServiceEnum::ServiceA(a)=> Arc::clone(&a),
+            _ => panic!("Not a ServiceA"),
+        }
+    }
+
+    fn service_b(c: &mut ContainerWithEnumDispatch) -> Arc<ServiceB> {
+        match c.build("service_b", |container: &mut ContainerWithEnumDispatch| {
+            ServiceEnum::ServiceB(Arc::from(ServiceB{service_a: service_a(container)}))
+        }).deref() {
+            ServiceEnum::ServiceB(a) => Arc::clone(&a),
+            _ => panic!("Not a ServiceB"),
+        }
+    }
+
+
+    #[test]
+    fn fetch_simple_service_from_bottom() {
+        let c = &mut ContainerWithEnumDispatch::new();
+        let service_a_instance = service_a(c);
+        let service_b_instance = service_b(c);
+        assert_eq!(service_b_instance.service_a.uuid, service_a_instance.uuid);
+    }
+}
